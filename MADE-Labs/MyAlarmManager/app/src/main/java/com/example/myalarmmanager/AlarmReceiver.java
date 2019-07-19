ef17004259a2eeb0c84cc2cf48974caacddc8b8e
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2019-07-19 20:23:28 +0700

    a

--- MADE-Labs/MyAlarmManager/app/src/main/java/com/example/myalarmmanager/AlarmReceiver.java
+++ MADE-Labs/MyAlarmManager/app/src/main/java/com/example/myalarmmanager/AlarmReceiver.java
@@ -1,10 +1,18 @@
 package com.example.myalarmmanager;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -33,6 +41,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         int notif = type.equalsIgnoreCase(TYPE_ONE_TIME) ? ID_ONETIME : ID_REPEATING;
 
         showToast(context, title, msg);
+        showAlarmNotification(context, title, msg, notif);
     }
 
     private void showToast(Context context, String title, String msg) {
@@ -82,6 +91,39 @@ public class AlarmReceiver extends BroadcastReceiver {
             return false;
         }catch (ParseException e){
             return true;
+        }
+    }
+
+    private void showAlarmNotification(Context context, String title, String message, int notifId){
+        String CHANNEL_ID = "Channel_1";
+        String CHANNEL_NAME = "AlaramManager channel";
+
+        NotificationManager notificationManagerCompat = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alaramSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_access_time_24px)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setSound(alaramSound);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{1000, 1000, 1000, 1000, 1000});
+            builder.setChannelId(CHANNEL_ID);
+            if (notificationManagerCompat != null) {
+                notificationManagerCompat.createNotificationChannel(channel);
+            }
+        }
+
+        Notification notification = builder.build();
+
+        if (notificationManagerCompat != null) {
+            notificationManagerCompat.notify(notifId, notification);
         }
     }
 }
--- MADE-Labs/MyAlarmManager/app/src/main/java/com/example/myalarmmanager/MainActivity.java
+++ MADE-Labs/MyAlarmManager/app/src/main/java/com/example/myalarmmanager/MainActivity.java
@@ -2,12 +2,101 @@ package com.example.myalarmmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DatePickerFragment.DialogDateListener, TimePickerFragment.DialogTimeListener {
+
+    TextView tvOnceDate, tvOnceTime, tvRepeatTime;
+    EditText edtOnceMessage, edtRepeatMessage;
+    ImageButton btnOnceDate , btnOnceTime, btnRepeatTime;
+    Button btnSetOnce, btnSetRepeat, btnCancelRepeat;
+
+    final String DATE_PICKER_TAG = "DatePicker";
+    final String TIME_PICKER_ONCE_TAG = "TimePickerOnce";
+    final String TIME_PICKER_REPEAT_TAG = "TimePickerRepeat";
+
+    private AlarmReceiver alarmReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvOnceDate = findViewById(R.id.tv_once_date);
+        tvOnceTime = findViewById(R.id.tv_once_clock);
+        tvRepeatTime = findViewById(R.id.tv_repeat_time);
+        edtOnceMessage = findViewById(R.id.edt_once_message);
+        edtRepeatMessage = findViewById(R.id.edt_repeat_message);
+        btnOnceDate = findViewById(R.id.btn_once_date);
+        btnOnceTime = findViewById(R.id.btn_once_time);
+        btnRepeatTime = findViewById(R.id.btn_repeat_time);
+        btnSetOnce = findViewById(R.id.btn_set_time_alarm);
+        btnSetRepeat = findViewById(R.id.btn_set_repeat_alarm);
+        btnCancelRepeat = findViewById(R.id.btn_cancel_repeat_alarm);
+
+        btnOnceDate.setOnClickListener(this);
+        btnOnceTime.setOnClickListener(this);
+        btnSetOnce.setOnClickListener(this);
+        btnRepeatTime.setOnClickListener(this);
+        btnSetRepeat.setOnClickListener(this);
+        btnCancelRepeat.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_once_date:
+                DatePickerFragment datePickerFragment = new DatePickerFragment();
+                datePickerFragment.show(getSupportFragmentManager(), DATE_PICKER_TAG);
+                break;
+            case R.id.btn_once_time:
+                TimePickerFragment timePickerFragment = new TimePickerFragment();
+                timePickerFragment.show(getSupportFragmentManager(), TIME_PICKER_ONCE_TAG);
+                break;
+            case R.id.btn_set_time_alarm:
+                String onceDate = tvOnceDate.getText().toString();
+                String onceTime = tvOnceTime.getText().toString();
+                String onceMessage = edtOnceMessage.getText().toString();
+                alarmReceiver.setOneTimeAlarm(this, AlarmReceiver.TYPE_ONE_TIME,onceDate,onceTime,onceMessage);
+                break;
+            case R.id.btn_repeat_time:
+                break;
+            case R.id.btn_set_repeat_alarm:
+                break;
+            case R.id.btn_cancel_repeat_alarm:
+                break;
+        }
+    }
+
+
+    @Override
+    public void onDialogDateSet(String tag, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, dayOfMonth);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        tvOnceDate.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    @Override
+    public void onDialogTimeSet(String tag, int hourOfDay, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        switch (tag){
+            case TIME_PICKER_ONCE_TAG:
+                tvOnceTime.setText(dateFormat.format(calendar.getTime()));
+                break;
+            default:
+                break;
+        }
     }
 }
