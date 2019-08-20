71de3a947dcc0c19ce81ce37934314ccb0e2e4e4
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2019-08-21 00:51:04 +0700

    Final Submission (18%)
    - Added new module for client app

---
+++ Movies/favorite/src/main/java/com/example/favorite/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.favorite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
